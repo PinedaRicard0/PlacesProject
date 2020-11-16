100d8d5fcd99d65676d3b720ffcf871ea1fbd39a
Ricardo Pineda Rubio <pinedaricardo07@gmail.com>
2020-11-16 14:10:55 -0500

    Unit tests
    Adding some unit test to the application

---
+++ ServicesTest/Controllers_UnitTests.cs
@@ -0,0 +1,42 @@
+﻿using IServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Views.Controllers;
+
+namespace ServicesTest
+{
+    [TestClass]
+    public class Controllers_UnitTests
+    {
+        private Mock<IPlacesService> _placesServiceMock;
+
+        private MunicipalityController GetMunicipalityController() {
+            _placesServiceMock = new Mock<IPlacesService>();
+            var municipalityCtrl = new MunicipalityController(_placesServiceMock.Object);
+            return municipalityCtrl;
+        }
+
+        private RegionController GetRegionController()
+        {
+            _placesServiceMock = new Mock<IPlacesService>();
+            var regionCtrl = new RegionController(_placesServiceMock.Object);
+            return regionCtrl;
+        }
+
+        [TestMethod]
+        public void RequestAllMunicipalities_CallPlacesServiceGetAllMunicipalities()
+        {
+            var municipalityCtrl = GetMunicipalityController();
+            var result = municipalityCtrl.Index();
+            _placesServiceMock.Verify(m => m.GetAllMunicipalities(), Times.Once);
+        }
+
+        [TestMethod]
+        public void RequestAllRegions_CallPlacesServiceGetAllRegion()
+        {
+            var RegionCtrl = GetRegionController();
+            var result = RegionCtrl.Index();
+            _placesServiceMock.Verify(m => m.GetAllRegions(), Times.Once);
+        }
+    }
+}
---
+++ ServicesTest/PlacesService_UnitTests.cs
@@ -0,0 +1,46 @@
+﻿using IDataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using Moq;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesTest
+{
+    [TestClass]
+    public class PlacesService_UnitTests
+    {
+        private Mock<IRegionRepo> _regionRepoMock;
+        private Mock<IRegionMunicipalitiesRepo> _regionMunicipalityRepoMock;
+        private Mock<IMunicipalityRepo> _municipalityRepoMock;
+
+        private PlacesService GetService() {
+            _regionRepoMock = new Mock<IRegionRepo>();
+            _regionMunicipalityRepoMock = new Mock<IRegionMunicipalitiesRepo>();
+            _municipalityRepoMock = new Mock<IMunicipalityRepo>();
+            var res = new PlacesService(_regionRepoMock.Object, _municipalityRepoMock.Object, _regionMunicipalityRepoMock.Object);
+            return res;
+        }
+
+
+        [TestMethod]
+        public void RequestAllMunicipalities_CallMunicipalitiesRepo() {
+            PlacesService service = GetService();
+            var result = service.GetAllMunicipalities();
+            _municipalityRepoMock.Verify(m => m.GetMunicipalities(), Times.Once);
+        }
+
+        [TestMethod]
+        public void RequestAllMunicipalities_GetAllMunicipalities()
+        {
+            PlacesService service = GetService();
+            var result = service.GetAllMunicipalities();
+            Assert.IsInstanceOfType(result, typeof(Task<List<Municipality>>));
+        }
+
+    }
+}
